9515ba3a85866ff093ccd84ac237a43fa3f2f643
Marius <grausas@gmail.com>
2021-01-14 20:50:49 +0200

    count for lecturers and courses

--- src/routes.js
+++ src/routes.js
@@ -404,7 +404,10 @@ router.delete("/deletegroup/:id", middleware.isLoggedIn, (req, res) => {
 router.get("/counts", middleware.isLoggedIn, (req, res) => {
   database((db) =>
     db.query(
-      `SELECT COUNT(id) as studentsCount from students`,
+      `SELECT
+  (SELECT COUNT(id) FROM students) as studentsCount, 
+  (SELECT COUNT(id) FROM lecturers ) as lecturersCount,
+  (SELECT COUNT(id) FROM courses) as coursesCount`,
       (err, result) => {
         if (err) {
           res.status(400).json(err);
